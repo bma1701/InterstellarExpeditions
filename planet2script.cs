aa80640f812fe4f6fefc853e3dac9bd2239f6fa0
davidpauld12 <55264420+davidpauld12@users.noreply.github.com>
2023-04-24 10:46:57 -0500

    Planet Level trigger on collision
    I made it where once the player collides with the planet it will load up the level the level has no design yet it's just a blank canvas. Just make sure the Playership is tagged as Player.

---
+++ planet2script.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class planet2script : MonoBehaviour
+{
+    // Start is called before the first frame update
+  [SerializeField]
+    private string scene;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+            SceneManager.LoadScene(scene);
+    }
+}
---
+++ planet3script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class planet3script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private string scene;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+            SceneManager.LoadScene(scene);
+    }
+   
+}
